b529c78b3ec82c2157d3d7ece9faee971f119ae0
unknown <arizqipstudio@gmail.com>
2014-04-10 12:13:57 +0700

    little progress

--- application/controllers/anggota.php
+++ application/controllers/anggota.php
@@ -2,4 +2,36 @@
 
 class Anggota extends CI_Controller {
 
+  public function index(){
+
+  }
+
+  public function viewAnggota(int idAnggota){
+    $this->load->model('Anggota_Model');
+    $data = $this->Anggota_Model->getDataAnggota($index);
+
+    $this->load->view('anggota',$data);
+  }
+
+  public function validateSignUp(){
+    $data = $this->input->post('daftarbaru');
+
+    $username = $data['user'];
+    $pass = $data['password'];
+    $confpass = $data['passconfirm'];
+
+    if($pass != $confpass){
+      $this->load->view('notifikasi','Konfirmasi password tidak sama');
+    }
+
+    if (strlen($username) < 4){
+      $this->load->view('notifikasi','Username harus lebih dari 4 karakter');
+    } else if (!preg_match("/[A-Za-z][A-Za-z0-9_]*/",)){
+      $this->load->view('notifikasi','Username harus diawali huruf');
+    }
+
+    if(strlen($pass)<6||strlen($pass)>24){
+      $this->load->view('notifikasi','Panjang password tidak valid');
+    }
+  }
 }
