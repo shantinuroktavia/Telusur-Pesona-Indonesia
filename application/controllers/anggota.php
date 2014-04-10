22898b81bda1f95fc685ac91f1aec8fada1c908b
ARizqiP <arizqipstudio@gmail.com>
2014-04-10 14:19:32 +0700

    Update anggota.php

--- application/controllers/anggota.php
+++ application/controllers/anggota.php
@@ -21,9 +21,10 @@ class Anggota extends CI_Controller {
 
   public function validateSignUp($data){
 
-    $username = $data['user'];
+    $username = $data['username'];
     $pass = $data['password'];
     $confpass = $data['passconfirm'];
+    $email = $data['emailaddress'];
     $valid=true;
 
     if($pass != $confpass){
@@ -43,6 +44,13 @@ class Anggota extends CI_Controller {
       $errmess['pesan'] = 'Panjang password tidak valid');
       $valid=false;
     }
+    
+    if (!preg_match("/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/", $email)) {
+      
+      $errmess['pesan'] = 'Alamat email tidak valid');
+      $valid=false;
+    }
+    
      if($valid){
        $this->load->model('Anggota_Model');
        $this->Anggota_Model->registerNewAnggota($data);
